ff22a957f5d7fa8efc607e3f639dad7a64b9f994
Guilherme <santanagui40@gmail.com>
2021-02-27 13:33:05 -0300

    project finalized

---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { MdWbSunny } from 'react-icons/md';
+import { FaMoon } from 'react-icons/fa';
+import styles from '../styles/components/Profile.module.css';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+export function Profile() {
+  const { toggleTheme, currentTheme } = useContext(ThemeContext);
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileRow}>
+      <div className={styles.profileContainer}>
+        <img src="https://github.com/guisantanaa.png" alt="Gui Santana" />
+        <div>
+          <strong>Guilherme Santana</strong>
+          <p>
+            <img src="icons/level.svg" alt="Level" />
+            Level {level}
+          </p>
+        </div>
+      </div>
+      {currentTheme === 'theme-dark' ? (
+        <MdWbSunny onClick={toggleTheme} />
+      ) : (
+        <FaMoon color="#2E384D" onClick={toggleTheme} />
+      )}
+    </div>
+  );
+}
